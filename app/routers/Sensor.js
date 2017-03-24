93f0aa50fb3485ff1b7daa1c70ae68ad39299d35
Rodrigo Sales <rsa.rodrigosales@gmail.com>
2017-03-23 23:11:44 -0300

    CRUD

--- app/routers/Sensor.js
+++ app/routers/Sensor.js
@@ -4,13 +4,63 @@ var router = express.Router();
 var Sensor = require(appRoot + '/app/models/Sensor.js');
 
 router.get('/', function(req, res) {
-		
+		console.log("get");
 		Sensor.find({}, function(err, sensors){
+			if(err){
+				console.log("Error");
+				res.send(err);
+
+			}
+			console.log("OK");
+			res.json(sensors);
+		});
+});
+
+router.get('/:sensor_id', function(req, res) {
+	
+		console.log("get by id");
+	
+		Sensor.findById(req.params.sensor_id, function(err, sensors){
+			if(err){
+				console.log("Error");
+				res.send(err);
+
+			}
+			console.log("OK");
+			res.json(sensors);
+		});
+});
+
+router.delete('/:sensor_id', function(req, res) {
+	
+		console.log("delete by id");
+	
+		Sensor.remove({_id: req.params.sensor_id}, function(err, sensors){
+			if(err){
+				console.log("Error");
+				res.send(err);
+
+			}
+			console.log("OK");
+			res.json({message: "Deleted" });
+		});
+});
+
+router.post('/', function(req, res) {
+		console.log("post");
+		var sensor = new Sensor();
+
+		sensor.name = req.body.name;
+
+		sensor.save(function(err){
 			if(err){
 				res.send(err);
 			}
-			res.json(sensors);
+
+			res.json({message: "sensor created!"});
+			console.log("OK");
 		});
+		
 });
 
 /*
